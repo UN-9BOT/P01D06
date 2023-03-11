209057b35b633c54f73e08279cc622ba43b47c84
Yajaira Hercules <un.9bot@gmail.com>
2023-03-11 15:03:00 +0300

    tee

--- src/pong.c
+++ src/pong.c
@@ -2,14 +2,14 @@
 #define HT 25
 #define WT 80
 
-void draw(int, int);
+void draw(int, int, int, int);
 int checkSP(int, int, int);
 // int field_rackets(int, int);
 int main(void) {
-    int x, y, spX, spY, rack;
+    int x, y, spX, spY, rack1, rack2;
 
     x = y = spX = spY = 2; 
-    draw(x, y);
+    draw(x, y, 12, 12);
     printf("\n");
     while (1) {
         spX = checkSP(x, spX, WT);
@@ -17,14 +17,16 @@ int main(void) {
         x += spX;
         y += spY;
         // rack = field_rackets(x, y); 
-        rack = 1;
-        draw(
+        rack1 = 6;
+        rack2 = 12;
+        draw(x, y, rack1, rack);
+
         getchar();
     }
     return (0);
 }
 
-void draw(int x, int y) { 
+void draw(int x, int y, int rack1, int rack2) { 
     for (int k = 0; k <= HT; k++) {  // y
         if (k == y) { // y 
             for (int j = 0; j <= WT; j++) {  // x
