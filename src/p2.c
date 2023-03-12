211859f0ad86225e13801d631ec9cea738fef43d
Yajaira Hercules <un.9bot@gmail.com>
2023-03-12 16:29:56 +0300

    pong 99.0

--- src/p2.c
+++ src/p2.c
@@ -8,36 +8,41 @@ int checkSPforHT(int, int, int);
 int check_click(char);
 int checkLose(int, int);
 void prCounter(int, int);
+void prWinner(int);
 
 int main(void) {
+    int flag;
     int ballX, ballY;        // координаты мяча
     int spX, spY;            // направление
     int counter1, counter2;  // счетчик результатов
-    int rack1, rack2;        //координаты оп оси у у ракеток
-    char var;                // symbol for input
+    int rack1, rack2;        // координаты оп оси у у ракеток
 
     ballX = 40;
     ballY = 13;
     spX = spY = 1;
-    counter1 = counter2 = 0;
+    counter1 = counter2 = flag = 0;
     rack1 = rack2 = 13;
 
     while (counter1 != 21 && counter2 != 21) {
-        scanf("%c", &var);
-        // printf("\r");
-        if (var == 'a' || var == 'z') {
+        char var;  // symbol for input
+        var = getchar();
+        if ((var == 'a' || var == 'z') && flag != 1) {
             if (rack1 - check_click(var) != 2 && rack1 - check_click(var) != 24) {
                 rack1 = rack1 - check_click(var);
+                flag = 1;
             }
-        } else if (var == 'k' || var == 'm') {
+        } else if ((var == 'k' || var == 'm') && flag != 2) {
             if (rack2 - check_click(var) != 2 && rack2 - check_click(var) != 24) {
                 rack2 = rack2 - check_click(var);
+                flag = 2;
             }
-        }  // else if (var == ' ') {
-           //  rack1 = rack1;
-           //  rack2 = rack2;
-        //}
 
+        } else if (var == ' ' && flag == 1) {
+            flag = 2;
+        } else if (var == ' ' && flag == 2) {
+            flag = 1;
+        }
+        draw(ballX, ballY, rack1, rack2);
         spX = checkSPforWT(ballX, spX, WT);
         spY = checkSPforHT(ballY, spY, HT);
         ballX += spX;
@@ -50,6 +55,7 @@ int main(void) {
         }
         prCounter(counter1, counter2);
     }
+    prWinner((counter1 > counter2) ? 1 : 2);
 }
 
 void draw(int ballX, int ballY, int rack1, int rack2) {
@@ -62,14 +68,14 @@ void draw(int ballX, int ballY, int rack1, int rack2) {
         } else {
             for (int x = 1; x <= WT; x++) {
                 if ((rack1 == y || rack1 + 1 == y || rack1 - 1 == y) && x == 1) {
-                    printf("|");    
+                    printf("|");
                 } else if ((rack2 == y || rack2 + 1 == y || rack2 - 1 == y) && x == 80) {
                     printf("|");
                 } else if (ballX == x && ballY == y) {
                     printf("o");
                 } else {
                     printf(" ");
-                } 
+                }
             }
             printf("\n");
         }
@@ -84,35 +90,72 @@ int checkSPforHT(int coor, int sp, int range) {
 }
 
 int checkLose(int ballY, int rack) {
+    int res;
     if (ballY != rack && ballY != rack + 1 && ballY != rack - 1) {
-        return (1);
+        res = 1;
     } else
-        return (0);
-    return (0);
+        res = 0;
+    return (res);
 }
 
 int check_click(char var) {
+    int res;
     if (var == 'a' || var == 'k') {
-        return 1;
+        res = 1;
     } else if (var == 'z' || var == 'm') {
-        return -1;
+        res = -1;
     } else if (var == ' ') {
-        return 0;
+        res = 0;
     } else
-        return 0;  //для обработки ошибок
+        res = 0;
+    return (res);
 }
 
 void prCounter(int c1, int c2) {
     for (int i = 0; i < 15; i++) {
-    printf(" "); }
+        printf(" ");
+    }
     printf("Player1");
     for (int i = 0; i < 15; i++) {
-    printf(" "); }
+        printf(" ");
+    }
     printf("%i || %i", c1, c2);
     for (int i = 0; i < 15; i++) {
-    printf(" "); }
+        printf(" ");
+    }
     printf("Player2");
     for (int i = 0; i < 15; i++) {
-    printf(" "); }
+        printf(" ");
+    }
     printf("\n\r");
+}
+
+void prWinner(int win) {
+    for (int y = 1; y <= HT; y++) {  // Ось ракетки
+        if (y == 1 || y == HT) {
+            for (int i = 1; i <= WT; i++) {  // Ось границ
+                printf("%c", '-');
+            }
+            printf("\n");
+        } else {
+            for (int x = 1; x <= WT; x++) {
+                if ((13 == y || 14 == y || 12 == y) && x == 1) {
+                    printf("|");
+                } else if ((13 == y || 14 == y || 12 == y) && x == WT) {
+                    printf("|");
+                } else if (y == 13 && x == 33) {
+                    if (win == 1) {
+                        printf("Winner is Player1");
+                        x += 16;
+                    } else {
+                        x += 16;
+                        { printf("Winner is Player2"); }
+                    }
+                } else {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+    }
 }
